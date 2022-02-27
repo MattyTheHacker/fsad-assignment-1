4c0ef78f43d668bb58574dd7e0571feb1bab4c04
Matty <matty258987@gmail.com>
2022-02-27 20:21:21 +0000

    almost done

--- src/main/java/fsd/assignment/assignment1/EditStudentController.java
+++ src/main/java/fsd/assignment/assignment1/EditStudentController.java
@@ -4,46 +4,35 @@ import fsd.assignment.assignment1.datamodel.Student;
 import fsd.assignment.assignment1.datamodel.StudentData;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 
 public class EditStudentController {
+    private final String[] modChoices = {"OOP", "Data Algo", "DS", "Maths", "AI",
+            "Adv Programming", "Project"};
     @FXML
     private Label yearStudyDisplay;
-
     @FXML
     private Label mod1Edit;
-
     @FXML
     private Label mod2Edit;
-
     @FXML
     private Label mod3Edit;
-
     @FXML
     private ChoiceBox<String> mod1ChoiceEdit;
-
     @FXML
     private ChoiceBox<String> mod2ChoiceEdit;
-
     @FXML
     private ChoiceBox<String> mod3ChoiceEdit;
-
-    //the modChoices variables correspond to the []
     private String mod1S, mod2S, mod3S;
 
-    private String[] modChoices = {"OOP", "Data Algo", "DS", "Maths", "AI",
-            "Adv Programming", "Project"};
-
     public void initialize() {
+        mod1ChoiceEdit.getItems().addAll(modChoices);
+        mod2ChoiceEdit.getItems().addAll(modChoices);
+        mod3ChoiceEdit.getItems().addAll(modChoices);
 
-        /* TODO: add all the modChoices to each ChoiceBox
-         */
-        //insert 3 lines of code here
-        //these lines have been given to you includes the setOnAction if a ChoiceBox is selected
         mod1ChoiceEdit.setOnAction(this::getChoiceEdit);
         mod2ChoiceEdit.setOnAction(this::getChoiceEdit);
         mod3ChoiceEdit.setOnAction(this::getChoiceEdit);
@@ -51,35 +40,24 @@ public class EditStudentController {
 
     //to ensure that detail pops up to edit
     public void setToEdit(Student stu) {
-        /* TODO: display the student to be edited details
-         */
-        //insert 4 lines of code here
-        /* TODO: get the new module choices using mod1S, mod2S and mod3S
-         */
-        mod1S = null;
-        mod2S = null;
-        mod3S = null;
+        yearStudyDisplay.setText(stu.getYearOfStudy());
+        mod1Edit.setText(stu.getModule1());
+        mod2Edit.setText(stu.getModule2());
+        mod3Edit.setText(stu.getModule3());
+
+        mod1S = mod1ChoiceEdit.getSelectionModel().getSelectedItem();
+        mod2S = mod2ChoiceEdit.getSelectionModel().getSelectedItem();
+        mod3S = mod3ChoiceEdit.getSelectionModel().getSelectedItem();
     }
 
     public Student processEdit(Student stu) {
-        /* TODO: complete the observableIst statement by getting all students from StudentData
-                 to be collected in allStudents
-         */
-        ObservableList<Student> allStudents = null;
-        /* TODO: get studId and year of study
-         */
-        String studIdS = null;
-        String yearStudyS = null;
-        /* TODO: remove the student to be edited from allStudents
-         */
-        //insert the line to remove the student here
-        /* TODO: add the student back by creating a new object reference and calling the addStudentData()
-         */
-        Student changedStu = null;
-        //call the addStudentData()
-        /* TODO: return the newly edited student back to the Controller class / editStudent()
-         */
-        return null;
+        ObservableList<Student> allStudents = StudentData.getInstance().getStudents();
+        String studIdS = stu.getStudId();
+        String yearStudyS = stu.getYearOfStudy();
+        allStudents.remove(stu);
+        Student changedStu = new Student(studIdS, yearStudyS, mod1S, mod2S, mod3S);
+        StudentData.getInstance().addStudentData(changedStu);
+        return changedStu;
     }
 
     public void getChoiceEdit(ActionEvent event) {
